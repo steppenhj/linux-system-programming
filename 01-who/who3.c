5718cc86f107b53247750a2e0b34424a8e7cad9a
steppenhj <phj1675@naver.com>
2026-04-08 22:19:42 +0900

    feat: utmplib.c + who3.c => Buffer 이용

---
+++ 01-who/who3.c
@@ -0,0 +1,58 @@
+// who3.c 
+// Buffer 사용. 
+// utmplib 
+
+#include <stdio.h>
+#include <utmp.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define SHOWHOST
+
+void showtime(long);
+void show_info(struct utmp *);
+
+int main(){
+	struct utmp *utbufp;
+	struct utmp *utmp_next();
+
+	if(utmp_open(UTMP_FILE)==-1){
+		perror(UTMP_FILE);
+		exit(1);
+	}
+
+	while((utbufp = utmp_next()) != ((struct utmp*) NULL))
+		show_info(utbufp);
+
+	utmp_close();
+
+	return 0;
+}
+
+void show_info(struct utmp* utbufp){
+	if(utbufp->ut_type != USER_PROCESS)
+		return;
+
+	printf("%-8.8s", utbufp->ut_name);
+	printf(" ");
+	printf("%-8.8s", utbufp->ut_line);
+	printf(" ");
+	
+	showtime(utbufp->ut_time);
+	printf(" ");
+
+#ifdef SHOWHOST
+	printf(" (%s)", utbufp->ut_host);
+#endif
+	printf("\n");
+}
+
+void showtime(long timeval){
+	char* cp;
+	cp = ctime(&timeval);
+
+	printf("%12.12s", cp+4);
+}
+
